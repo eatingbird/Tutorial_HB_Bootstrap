f7214df516a6e7c0baf455e5b62034e1b1d7e824
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-10 17:25:02 -0800

    Containers and rows added

---
+++ melon_price.py
@@ -0,0 +1,11 @@
+melon_price = {
+    'cantaloupe': {'price': 4.99,
+                   'color': 'Tan',
+                   'country': 'Italy'},
+    'honeydew': {'price': 3.99,
+                 'color': 'White/Green',
+                 'country': 'France/Algeria'},
+    'casaba': {'price': 7.99,
+               'color': 'Yellow',
+               'country': 'Turkey'}
+    }
